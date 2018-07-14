b04125120dfb6319d1cfacf1d9d4b857068863db
Anastasios Chondrogiannis <info@anastasioscho.gr>
2018-07-14 15:26:27 +0300

    feat: Limit the URLs that the service worker can control
    This fixes the following error:
    
    Uncaught (in promise) TypeError: Request scheme 'chrome-extension' is unsupported

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
 const cacheName = 'urr-static-v1';
 
+const allowedURLOrigins = [
+    location.origin,
+    'https://api.tiles.mapbox.com',
+    'https://unpkg.com'
+];
+
 self.addEventListener('fetch', function(event) {
+    const requestURL = new URL(event.request.url);
+    if (!allowedURLOrigins.includes(requestURL.origin)) return;
+
     event.respondWith(
         caches.open(cacheName).then(function(cache) {
             return cache.match(event.request).then(function(response) {
